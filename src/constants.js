544680163106fd4bff4372b670f1fa23e4cdcf37
Tom Nolan <tsnolan@outlook.com>
2019-05-25 12:52:41 -0500

    refactor: use env variables

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,9 @@
-export const GOOGLE_API_KEY = "AIzaSyAtl-FpdnoUALstN7scI85Ec9HiVzm7n9Q";
+import {
+  GOOGLE_API_KEY as GAPIKEY,
+  SERVER_IP_ADDRESS
+} from "react-native-dotenv";
+
+export const GOOGLE_API_KEY = GAPIKEY;
 export const PLACES_SEARCH_BASE_URL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_API_KEY}`;
 export const DIRECTIONS_BASE_URL = `https://maps.googleapis.com/maps/api/directions/json?key=${GOOGLE_API_KEY}`;
-export const SOCKET_BASE_URL = "http://192.168.1.5:3000";
+export const SOCKET_BASE_URL = `http://${SERVER_IP_ADDRESS}:3000`;
